f8ce3d296ed53ef58d039a13e0ba1863dc90fe7f
dias-olr <rdiasoli97@gmail.com>
2021-01-07 07:08:26 +0000

    Adding getData route which will return all data thanks to the settings given by the user client.

--- routes/index.js
+++ routes/index.js
@@ -36,4 +36,85 @@ async function readFile(path) {
 	}
 }
 
+/**
+ * @GET / READ
+ * @Route("/getData/:config")
+ */
+router.get('/getData/:config', async (request, response) => {
+	try {
+		const data = {};
+		const config = JSON.parse(request.params.config);
+		config.dataPath = basePath + config.dataPath;
+
+		const dirents = await readDirectory(config.dataPath);
+		const directories = dirents.filter(file => file.isDirectory());
+
+		let filesPerDirectory = await Promise.all(directories.map((directory) => readDirectory(config.dataPath + directory.name)));
+
+		filesPerDirectory = filesPerDirectory.reduce((arr, files) => {
+			const f = files.filter(file => file.isFile() && file.name.match(new RegExp(config.filePattern, "g")));
+			if (f.length > 0) arr.push(f);
+			return arr;
+		}, []);
+
+		filesPerDirectory.forEach((filesInDirectory, i) => {
+			var collator = new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' });
+			filesPerDirectory[i] = filesInDirectory.sort((fileA, fileB) => fileA.name.localeCompare(fileB.name, undefined, {
+				numeric: true,
+				sensitivity: 'base'
+			}));
+		});
+
+		const filesContentPerDirectory = await Promise.all(filesPerDirectory.map((directory, i) => {
+			return Promise.all(directory.map((file) => readFile(config.dataPath + directories[i].name + "/" + file.name)));
+		}));
+
+		data["directories"] = [];
+		filesPerDirectory.forEach((filesInDirectory, i) => {
+			const obj = {};
+			obj["directoryName"] = directories[i].name;
+			obj["files"] = [];
+
+			filesInDirectory.forEach((file, j) => {
+				const fileObj = {};
+				fileObj["filename"] = file.name;
+				fileObj["filedata"] = [];
+
+				const fileContentSplitted = filesContentPerDirectory[i][j].split(/\r?\n|\r/).map(line => line.split(config.delimiter));
+				if (fileContentSplitted.length > 0 && fileContentSplitted[fileContentSplitted.length - 1][0] === '') fileContentSplitted.pop();
+
+				fileContentSplitted.forEach((content, l) => {
+					if (l == 0) {
+						content.forEach((line, k) => {
+							const fileContent = {};
+							if (config.ignoreFirstLine) {
+								fileContent["columnName"] = line;
+								fileContent["data"] = [];
+								fileObj["filedata"].push(fileContent);
+							} else {
+								fileContent["columnName"] = "Column-" + (k + 1);
+								fileContent["data"] = [parseFloat(line)];
+								fileObj["filedata"].push(fileContent);
+							}
+						});
+					} else {
+						content.forEach((line, k) => {
+							fileObj["filedata"][k]["data"].push(parseFloat(line));
+						});
+					}
+				});
+
+				obj["files"].push(fileObj);
+				//if (data["folders"][i].length === 0) delete fileObj["fileContent"][i];
+			});
+			data["directories"].push(obj);
+			console.log(data);
+		});
+		response.status(200).json(data);
+	} catch (err) {
+		console.log(err);
+		response.status(400).json("Unable to parse with the settings received.\nError : " + err);
+	}
+});
+
 module.exports = router;
