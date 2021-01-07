3eee9ca700510c9d330b505f253b00058dc78280
dias-olr <ed.candido843@example.com>
2021-01-07 06:58:56 +0000

    Adding getDirectories route to get directories from public/data.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,39 @@
 const fs = require('fs');
 const router = require('express').Router();
+const basePath = 'public/data/';
+
+async function readDirectory(path) {
+	try {
+		return fs.promises.readdir(path, { "withFileTypes": true });
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+/**
+ * @GET / READ
+ * @Route("/getDirectories/")
+ */
+router.get('/getDirectories/', async function (request, response) {
+	try {
+		const files = await readDirectory(basePath);
+		const directories = files.reduce((finalArray, file) => {
+			if(file.isDirectory()) finalArray.push(file.name + "/");
+			return finalArray;
+		}, []);
+		console.log(directories);
+		response.status(200).json(directories);
+	} catch (err) {
+		response.status(500).json(err);
+	}
+});
+
+async function readFile(path) {
+	try {
+		return fs.promises.readFile(path, "UTF-8");
+	} catch (err) {
+		console.log(err);
+	}
+}
 
 module.exports = router;
